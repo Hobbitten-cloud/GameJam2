fb9a34804bd8f60a2db5aa5cd33e999f7b85e86d
Dennis Jensen <deje67370@edu.ucl.dk>
2025-10-20 13:11:56 +0200

    added some item

--- JourneyGame/Models/Item.cs
+++ JourneyGame/Models/Item.cs
@@ -1,23 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JourneyGame.Models
+﻿namespace JourneyGame.Models
 {
-    internal class Item
+    public class Item
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public int Value { get; set; }
-        public Item(int id, string name, string description, int value)
-        {
-            Id = id;
-            Name = name;
-            Description = description;
-            Value = value;
-        }
+
     }
 }
---
+++ JourneyGame/Models/ItemRepo.cs
@@ -0,0 +1,42 @@
+﻿namespace JourneyGame.Models
+{
+    public class ItemRepo
+    {
+        public List<Item> items = new List<Item>
+        {
+            new Item
+            {
+                Id = 1,
+                Name = "Bat",
+                Description = "An old Baseball bat",
+            },
+            new Item
+            {
+                Id = 2,
+                Name = "Ring of Power",
+                Description = "Your mothers ring ",
+
+            },
+            new Item
+            {
+                Id = 3,
+                Name = "Toothbrush",
+                Description = "A used toothbrush",
+
+            },
+            new Item
+            {
+                Id = 4,
+                Name = "Chair",
+                Description = "An Worn out chair",
+
+            }
+
+
+        };
+
+
+
+
+    }
+}

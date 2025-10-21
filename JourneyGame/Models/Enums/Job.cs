c4f2c298da37986fa55552241ae30384fb355510
Averyden <igo408gt@gmail.com>
2025-10-21 11:44:01 +0200

    Map the stats in such a sexy way

--- JourneyGame/Models/Enums/Job.cs
+++ JourneyGame/Models/Enums/Job.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JourneyGame.Models.Enums
+﻿namespace JourneyGame.Models.Enums
 {
     public enum Job
     {

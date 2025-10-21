9693fe9059ec3e430f02645d45ca7a89fd483f2e
Dennis Jensen <deje67370@edu.ucl.dk>
2025-10-21 10:21:20 +0200

    Add inventory and inventory methode

---
+++ JourneyGame/Models/Inventory.cs
@@ -0,0 +1,28 @@
+﻿namespace JourneyGame.Models
+{
+    public class Inventory
+    {
+        public List<Item> items = new List<Item>();
+        public void AddItem(string name, int id)
+        {
+            items.Add(new Item(id, name));
+
+        }
+        public void RemoveItem(string name)
+        {
+            var itemToRemove = items.FirstOrDefault(i => i.Name == name);
+            if (itemToRemove != null)
+            {
+                items.Remove(itemToRemove);
+            }
+
+        }
+        public void ShowIventory()
+        {
+            foreach (Item item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}

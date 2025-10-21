6daa7260feb68ffee0fa8a6becd818ad2792139b
BirkNP <Birk.p321@gmail.com>
2025-10-21 13:29:52 +0200

    list

--- JourneyGame/Program.cs
+++ JourneyGame/Program.cs
@@ -15,6 +15,8 @@ namespace JourneyGame
             Console.WriteLine();
             Console.WriteLine($"{test.Name} \n{test.Dialogue}");
 
+            List<Npc> npcs = new List<Npc>();
+
             var HumanPaladin = new Npc("Ramon", Models.Enums.Race.Human, "I used to be an adventurer like you, then I took a fireball in a tar pit");
             var HumanWarrior = new Npc("Jon Xina", Models.Enums.Race.Human, "I love ice cream!");
             var HumanMage = new Npc("Gandolf", Models.Enums.Race.Human, "How big do you think this room is?");
@@ -29,10 +31,29 @@ namespace JourneyGame
             var ElfMage = new Npc("Ash", Models.Enums.Race.Elf, "This is my BOOM stick");
             var ElfThief = new Npc("Fleet-Footed Fenty", Models.Enums.Race.Elf, "Wanna see me run to that mountain and back? Wanna see me do it again?");
             var GoblinMage = new Npc("Etienne", Models.Enums.Race.Goblin, "bogos binted 0.0");
-            var DwarfWarrior = new Npc("Gimli", Models.Enums.Race.Dwarf, "");
+            var DwarfWarrior = new Npc("Gimli", Models.Enums.Race.Dwarf, "You have my axe");
             var DwarfPaladin = new Npc("The Gilded One", Models.Enums.Race.Dwarf, "People keep making fun of my bronze hammer painted yellow");
             var DwarfMage = new Npc("Glowstick Jarn", Models.Enums.Race.Dwarf, "I like frost spells. They're very cool");
-            var GoblinArcher = new Npc("");
+            var GoblinArcher = new Npc("Dart", Models.Enums.Race.Goblin, "I once befriended a worg");
+            npcs.Add(HumanPaladin);
+            npcs.Add(HumanWarrior);
+            npcs.Add(HumanMage);
+            npcs.Add(HumanArcher);
+            npcs.Add(HumanThief);
+            npcs.Add(DwarfWarrior);
+            npcs.Add(DwarfPaladin);
+            npcs.Add(DwarfMage);
+            npcs.Add(OrcMage);
+            npcs.Add(OrcPaladin);
+            npcs.Add(OrcWarrior);
+            npcs.Add(OrcArcher);
+            npcs.Add(OrcThief);
+            npcs.Add(ElfArcher);
+            npcs.Add(ElfMage);
+            npcs.Add(ElfThief);
+            npcs.Add(GoblinMage);
+            npcs.Add(GoblinArcher);
+            npcs.Add(DwarfMage);
         }
     }
 }

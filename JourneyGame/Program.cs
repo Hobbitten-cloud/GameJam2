2a9841cdae013e8603e465ed5403580a7025d2e5
Averyden <igo408gt@gmail.com>
2025-10-22 08:01:16 +0200

    Make music play in background instead of taking up the entire process

--- JourneyGame/Program.cs
+++ JourneyGame/Program.cs
@@ -1,38 +1,62 @@
 ﻿using JourneyGame.Models;
 using NAudio.Wave;
+using System;
+using System.Threading.Tasks;
 
 namespace JourneyGame
 {
     public class Program
     {
+        private static WaveOutEvent? _outputDevice;
+        private static AudioFileReader? _audioFile;
+
         static void Main(string[] args)
         {
-			using (var audioFile = new AudioFileReader("Resources/Music/joe-biden-you-have-two-yil.mp3"))
-			using (var outputDevice = new WaveOutEvent())
-			{
-				outputDevice.Init(audioFile);
-				outputDevice.Play();
-
-				Console.WriteLine("Playing music...");
-				while (outputDevice.PlaybackState == PlaybackState.Playing)
-				{
-					System.Threading.Thread.Sleep(1000);
-				}
-			}
-			// NOTES 
-			// Ascii font used = Doom
-			// Website = https://patorjk.com/software/taag/#p=display&f=Doom&t=John+Journey&x=none&v=4&h=4&w=80&we=false
-
-			// Init game components here
-			var gameMenus = new Menu();
-            //var test = new Npc("Den store tester", Models.Enums.Race.Dwarf, "Du skal simpelthen opføre dig ordentligt!");
-
-            //Console.WriteLine($"{test.Name} \n{test.Dialogue}");
+            // Start music playback in a background thread
+            Task.Run(() => PlayMusic("Resources/Music/joe-biden-you-have-two-yil.mp3"));
 
 
-            // Game initialization and main loop would go here
-            //gameMenus.StartMenu();
+            // NOTES 
+            // Ascii font used = Doom
+            // Website = https://patorjk.com/software/taag/#p=display&f=Doom&t=John+Journey&x=none&v=4&h=4&w=80&we=false
+
+            // Initialize game components here
+            var gameMenus = new Menu();
             gameMenus.StartMenu();
+
+            // Optionally stop music when exiting
+            StopMusic();
+        }
+
+        private static void PlayMusic(string filePath)
+        {
+            try
+            {
+                _audioFile = new AudioFileReader(filePath);
+                _outputDevice = new WaveOutEvent();
+                _outputDevice.Init(_audioFile);
+                _outputDevice.Play();
+
+                // Keep thread alive while playing
+                while (_outputDevice.PlaybackState == PlaybackState.Playing)
+                {
+                    System.Threading.Thread.Sleep(500);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error playing music: {ex.Message}");
+            }
+        }
+
+        private static void StopMusic()
+        {
+            if (_outputDevice != null)
+            {
+                _outputDevice.Stop();
+                _outputDevice.Dispose();
+                _audioFile?.Dispose();
+            }
         }
     }
 }
